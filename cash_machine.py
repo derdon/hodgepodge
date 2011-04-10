e9e57ff0c87029cb117b82427aefa622afd7bcb1
derdon <liedtke.simon@googlemail.com>
2011-04-10 23:05:18 +0200

    better error handling in cash_machine.py

--- cash_machine.py
+++ cash_machine.py
@@ -3,7 +3,18 @@ import sys
 
 from texttable import Texttable
 
-def cash_machine(req_money, debug=False):
+
+class InvalidBanknote(ValueError):
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __str__(self):
+        return (
+            'An amount of %d was requested, '
+            'but it is not possible to give it out' % self.amount)
+
+
+def cash_machine(req_money):
     possible_bills = [5, 10, 20, 50, 100, 200, 500]
     output = list()
     money_out = 0
@@ -15,13 +26,10 @@ def cash_machine(req_money, debug=False):
                 possible_bills.pop()
                 req_money = rest
                 break
-    if debug:
-        if req_money in xrange(1, 5):
-            sys.stderr.write(
-                'ERROR:You requested an amount of %d but it is'
-                ' not possible to give it out.\n' % req_money
-            )
+    if req_money in xrange(1, 5):
+        raise InvalidBanknote(req_money)
     return output
+
 
 def table_output(output):
     table = Texttable()
@@ -31,9 +39,13 @@ def table_output(output):
     return table.draw()
 
 if __name__ == '__main__':
+    def my_excepthook(type, value, traceback):
+        print('ERROR: %s' % value)
+
+    sys.excepthook = my_excepthook
     try:
         req_money = int(sys.argv[1])
     except IndexError:
         req_money = int(raw_input('Enter the requested money: '))
-    output = cash_machine(req_money, debug=True)
-    print table_output(output)
+    output = cash_machine(req_money)
+    print(table_output(output))
