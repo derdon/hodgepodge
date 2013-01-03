b8516d99d4de0b566369b73dfb76cd9a289f1295
derdon <liedtke.simon@googlemail.com>
2013-01-03 22:25:59 +0100

    shortened haskell solution to euler problem #14

--- lang/challenges/euler-solutions/14/14.hs
+++ lang/challenges/euler-solutions/14/14.hs
@@ -7,10 +7,6 @@ collatzSequence n
     | even n = n : collatzSequence (n `div` 2)
     | otherwise = n : collatzSequence (3 * n + 1)
 
-allCollatzSequences :: (Integral a) => [a] -> [[a]]
-allCollatzSequences [] = []
-allCollatzSequences (x:xs) = collatzSequence x : allCollatzSequences xs
-
 main =
     let input = [500001,500003..999999]
-    in print $ head $ maximumBy (comparing length) $ allCollatzSequences input
+    in print $ head $ maximumBy (comparing length) $ map collatzSequence input
